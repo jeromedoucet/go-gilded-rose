d903554886a7b6ea9fa39fd843c9a4a1eb975349
Jerome Doucet <jerdct@gmail.com>
2018-09-15 16:01:52 +0200

    step 7

--- gilded-rose_test.go
+++ gilded-rose_test.go
@@ -48,3 +48,25 @@ func Test_GildedRose_backstage_sellin_10(t *testing.T) {
 		t.Errorf("expect the result to be %s but got, %s", expectedResult, result)
 	}
 }
+
+func Test_GildedRose_backstage_sellin_5(t *testing.T) {
+	// given
+	expectedResult := "[{+5 Dexterity Vest 9 19} {Aged Brie 1 1} {Elixir of the Mongoose 4 6} {Sulfuras, Hand of Ragnaros 0 80} {Backstage passes to a TAFKAL80ETC concert 4 23} {Conjured Mana Cake 2 5}]"
+
+	var givenItems = []Item{
+		Item{"+5 Dexterity Vest", 10, 20},
+		Item{"Aged Brie", 2, 0},
+		Item{"Elixir of the Mongoose", 5, 7},
+		Item{"Sulfuras, Hand of Ragnaros", 0, 80},
+		Item{"Backstage passes to a TAFKAL80ETC concert", 5, 20},
+		Item{"Conjured Mana Cake", 3, 6},
+	}
+
+	// when
+	result := fmt.Sprintf("%v", GildedRose(givenItems))
+
+	// then
+	if result != expectedResult {
+		t.Errorf("expect the result to be %s but got, %s", expectedResult, result)
+	}
+}
