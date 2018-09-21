4cc6c482596d6fca34e5d130ad14a950a6a962a2
Jerome Doucet <jerdct@gmail.com>
2018-09-21 15:09:04 +0200

    wip

--- gilded-rose.go
+++ gilded-rose.go
@@ -7,7 +7,7 @@ type Item struct {
 	sellIn, quality int
 }
 
-var items = []Item{
+var currentItems = []Item{
 	Item{"+5 Dexterity Vest", 10, 20},
 	Item{"Aged Brie", 2, 0},
 	Item{"Elixir of the Mongoose", 5, 7},
@@ -18,58 +18,54 @@ var items = []Item{
 
 func main() {
 	fmt.Println("OMGHAI!")
-	// fmt.Print(items)
-	GildedRose()
+	fmt.Println(currentItems)
+	currentItems = GildedRose(currentItems)
 }
 
-func GildedRose() {
+func GildedRose(items []Item) []Item {
 	for i := 0; i < len(items); i++ {
+		if items[i].name == "Sulfuras, Hand of Ragnaros" {
+			continue
+		}
+
+		if items[i].quality >= 50 {
+			items[i].sellIn = items[i].sellIn - 1
+			continue
+		}
 
 		if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
 			if items[i].quality > 0 {
-				if items[i].name != "Sulfuras, Hand of Ragnaros" {
-					items[i].quality = items[i].quality - 1
-				}
+				items[i].quality = items[i].quality - 1
 			}
 		} else {
-			if items[i].quality < 50 {
-				items[i].quality = items[i].quality + 1
-				if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].sellIn < 11 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
-						}
-					}
-					if items[i].sellIn < 6 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
-						}
-					}
+			items[i].quality = items[i].quality + 1
+			if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
+				if items[i].sellIn < 11 {
+					items[i].quality = items[i].quality + 1
+				}
+				if items[i].sellIn < 6 {
+					items[i].quality = items[i].quality + 1
 				}
 			}
 		}
 
-		if items[i].name != "Sulfuras, Hand of Ragnaros" {
-			items[i].sellIn = items[i].sellIn - 1
-		}
+		items[i].sellIn = items[i].sellIn - 1
 
 		if items[i].sellIn < 0 {
 			if items[i].name != "Aged Brie" {
 				if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
 					if items[i].quality > 0 {
-						if items[i].name != "Sulfuras, Hand of Ragnaros" {
-							items[i].quality = items[i].quality - 1
-						}
+						items[i].quality = items[i].quality - 1
 					}
 				} else {
 					items[i].quality = items[i].quality - items[i].quality
 				}
 			} else {
-				if items[i].quality < 50 {
-					items[i].quality = items[i].quality + 1
-				}
+				items[i].quality = items[i].quality + 1
 			}
 		}
 	}
+
+	return items
 
 }
--- gilded-rose_test.go
+++ gilded-rose_test.go
@@ -1,7 +1,153 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func Test_GildedRose(t *testing.T) {
-	main()
+func Test_GildedRose_with_original_set(t *testing.T) {
+	// given
+	var originalItems = []Item{
+		Item{"+5 Dexterity Vest", 10, 20},
+		Item{"Aged Brie", 2, 0},
+		Item{"Elixir of the Mongoose", 5, 7},
+		Item{"Sulfuras, Hand of Ragnaros", 0, 80},
+		Item{"Backstage passes to a TAFKAL80ETC concert", 15, 20},
+		Item{"Conjured Mana Cake", 3, 6},
+	}
+	expectedResult := "[{+5 Dexterity Vest 9 19} {Aged Brie 1 1} {Elixir of the Mongoose 4 6} {Sulfuras, Hand of Ragnaros 0 80} {Backstage passes to a TAFKAL80ETC concert 14 21} {Conjured Mana Cake 2 5}]"
+
+	// when
+	givenItems := GildedRose(originalItems)
+
+	// then
+	if fmt.Sprint(givenItems) != expectedResult {
+		t.Errorf("Expect to have %s when calling GildedRose() with no args but got %s", expectedResult, fmt.Sprint(givenItems))
+	}
+}
+
+func Test_GildedRose_with_backstage_when_concert_in_10_days(t *testing.T) {
+	// given
+	var originalItems = []Item{
+		Item{"+5 Dexterity Vest", 10, 20},
+		Item{"Aged Brie", 2, 0},
+		Item{"Elixir of the Mongoose", 5, 7},
+		Item{"Sulfuras, Hand of Ragnaros", 0, 80},
+		Item{"Backstage passes to a TAFKAL80ETC concert", 10, 20},
+		Item{"Conjured Mana Cake", 3, 6},
+	}
+	expectedResult := "[{+5 Dexterity Vest 9 19} {Aged Brie 1 1} {Elixir of the Mongoose 4 6} {Sulfuras, Hand of Ragnaros 0 80} {Backstage passes to a TAFKAL80ETC concert 9 22} {Conjured Mana Cake 2 5}]"
+
+	// when
+	givenItems := GildedRose(originalItems)
+
+	// then
+	if fmt.Sprint(givenItems) != expectedResult {
+		t.Errorf("Expect to have %s when calling GildedRose() with no args but got %s", expectedResult, fmt.Sprint(givenItems))
+	}
+}
+
+func Test_GildedRose_with_backstage_when_concert_in_5_days(t *testing.T) {
+	// given
+	var originalItems = []Item{
+		Item{"+5 Dexterity Vest", 10, 20},
+		Item{"Aged Brie", 2, 0},
+		Item{"Elixir of the Mongoose", 5, 7},
+		Item{"Sulfuras, Hand of Ragnaros", 0, 80},
+		Item{"Backstage passes to a TAFKAL80ETC concert", 5, 20},
+		Item{"Conjured Mana Cake", 3, 6},
+	}
+	expectedResult := "[{+5 Dexterity Vest 9 19} {Aged Brie 1 1} {Elixir of the Mongoose 4 6} {Sulfuras, Hand of Ragnaros 0 80} {Backstage passes to a TAFKAL80ETC concert 4 23} {Conjured Mana Cake 2 5}]"
+
+	// when
+	givenItems := GildedRose(originalItems)
+
+	// then
+	if fmt.Sprint(givenItems) != expectedResult {
+		t.Errorf("Expect to have %s when calling GildedRose() with no args but got %s", expectedResult, fmt.Sprint(givenItems))
+	}
+}
+
+func Test_GildedRose_when_elixir_out_of_date(t *testing.T) {
+	// given
+	var originalItems = []Item{
+		Item{"+5 Dexterity Vest", 10, 20},
+		Item{"Aged Brie", 2, 0},
+		Item{"Elixir of the Mongoose", -1, 7},
+		Item{"Sulfuras, Hand of Ragnaros", 0, 80},
+		Item{"Backstage passes to a TAFKAL80ETC concert", 15, 20},
+		Item{"Conjured Mana Cake", 3, 6},
+	}
+	expectedResult := "[{+5 Dexterity Vest 9 19} {Aged Brie 1 1} {Elixir of the Mongoose -2 5} {Sulfuras, Hand of Ragnaros 0 80} {Backstage passes to a TAFKAL80ETC concert 14 21} {Conjured Mana Cake 2 5}]"
+
+	// when
+	givenItems := GildedRose(originalItems)
+
+	// then
+	if fmt.Sprint(givenItems) != expectedResult {
+		t.Errorf("Expect to have %s when calling GildedRose() with no args but got %s", expectedResult, fmt.Sprint(givenItems))
+	}
+}
+
+func Test_GildedRose_when_brie_out_of_date(t *testing.T) {
+	// given
+	var originalItems = []Item{
+		Item{"+5 Dexterity Vest", 10, 20},
+		Item{"Aged Brie", -1, 0},
+		Item{"Elixir of the Mongoose", 5, 7},
+		Item{"Sulfuras, Hand of Ragnaros", 0, 80},
+		Item{"Backstage passes to a TAFKAL80ETC concert", 15, 20},
+		Item{"Conjured Mana Cake", 3, 6},
+	}
+	expectedResult := "[{+5 Dexterity Vest 9 19} {Aged Brie -2 2} {Elixir of the Mongoose 4 6} {Sulfuras, Hand of Ragnaros 0 80} {Backstage passes to a TAFKAL80ETC concert 14 21} {Conjured Mana Cake 2 5}]"
+
+	// when
+	givenItems := GildedRose(originalItems)
+
+	// then
+	if fmt.Sprint(givenItems) != expectedResult {
+		t.Errorf("Expect to have %s when calling GildedRose() with no args but got %s", expectedResult, fmt.Sprint(givenItems))
+	}
+}
+
+func Test_GildedRose_when_backstage_out_of_date(t *testing.T) {
+	// given
+	var originalItems = []Item{
+		Item{"+5 Dexterity Vest", 10, 20},
+		Item{"Aged Brie", -1, 0},
+		Item{"Elixir of the Mongoose", 5, 7},
+		Item{"Sulfuras, Hand of Ragnaros", 0, 80},
+		Item{"Backstage passes to a TAFKAL80ETC concert", -1, 20},
+		Item{"Conjured Mana Cake", 3, 6},
+	}
+	expectedResult := "[{+5 Dexterity Vest 9 19} {Aged Brie -2 2} {Elixir of the Mongoose 4 6} {Sulfuras, Hand of Ragnaros 0 80} {Backstage passes to a TAFKAL80ETC concert -2 0} {Conjured Mana Cake 2 5}]"
+
+	// when
+	givenItems := GildedRose(originalItems)
+
+	// then
+	if fmt.Sprint(givenItems) != expectedResult {
+		t.Errorf("Expect to have %s when calling GildedRose() with no args but got %s", expectedResult, fmt.Sprint(givenItems))
+	}
+}
+
+func Test_GildedRose_when_brie_has_value_50(t *testing.T) {
+	// given
+	var originalItems = []Item{
+		Item{"+5 Dexterity Vest", 10, 20},
+		Item{"Aged Brie", 2, 50},
+		Item{"Elixir of the Mongoose", 5, 7},
+		Item{"Sulfuras, Hand of Ragnaros", 0, 80},
+		Item{"Backstage passes to a TAFKAL80ETC concert", 15, 20},
+		Item{"Conjured Mana Cake", 3, 6},
+	}
+	expectedResult := "[{+5 Dexterity Vest 9 19} {Aged Brie 1 50} {Elixir of the Mongoose 4 6} {Sulfuras, Hand of Ragnaros 0 80} {Backstage passes to a TAFKAL80ETC concert 14 21} {Conjured Mana Cake 2 5}]"
+
+	// when
+	givenItems := GildedRose(originalItems)
+
+	// then
+	if fmt.Sprint(givenItems) != expectedResult {
+		t.Errorf("Expect to have %s when calling GildedRose() with no args but got %s", expectedResult, fmt.Sprint(givenItems))
+	}
 }
